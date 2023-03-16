ea7eca6dab4db2f11e0a61c03d988c2b52c053ea
Saad5400 <sdbtwa@gmail.com>
2023-03-16 13:39:31 +0300

    Add project files.

---
+++ PostHive/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PostHive.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
